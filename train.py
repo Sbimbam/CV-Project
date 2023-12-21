8d0e670a9284d5086647a84e5721f532363a6e59
Sbimbam <EMANUELE.CORONGIU@HOTMAIL.IT>
2023-12-21 22:56:21 +0100

    loading result training
    Upload the first results from the training with a small dataset

--- train.py
+++ train.py
@@ -3,14 +3,14 @@
 from ultralytics import YOLO
 import os
 
-dataset_folder = ''
+dataset_folder = r"D:\OneDrive - Università di Cagliari\3 ERASMUS\CORSI\CV\CV_PROJECT\CV-Project-GIT\Data\Datasets\first_dataset"
 
 # Reference: https://docs.ultralytics.com/modes/train/#usage-examples
 # load the model
 model = YOLO("yolov8n.yaml")
 #train the model
 result = model.train(
-    data = os.path.join(dataset_folder, 'data.yamal'), # define the dataset
-    epochs = 100,                                      # define the numer of epochs                          # set results path
+    data = os.path.join(dataset_folder, 'data.yaml'), # define the dataset
+    epochs = 10,                                      # define the numer of epochs                          # set results path
     plots = True                                       # save plots and images during train/val
 )
